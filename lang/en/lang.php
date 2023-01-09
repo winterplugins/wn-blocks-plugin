2595650df5c6a8bcfef805436ad989fbc95ee06a
Dmitry R <dimsog@gmail.com>
2023-01-09 17:01:59 +0300

    feat: init models

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -4,5 +4,12 @@ return [
     'plugin' => [
         'name' => 'Blocks',
         'description' => 'Visual block plugin for WinterCMS',
-    ]
+    ],
+    'models' => [
+        'general' => [
+            'id' => 'ID',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ],
+    ],
 ];
